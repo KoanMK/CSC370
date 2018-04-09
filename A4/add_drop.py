ba712dd2a4d98f7f0149c229d5b8b6a5ea8a712b
KoanMK <nigeldecontie@gmail.com>
2018-04-08 19:35:25 -0700

    yuh

--- A4/add_drop.py
+++ A4/add_drop.py
@@ -6,6 +6,16 @@
 
 import sys, csv, psycopg2
 
+psql_user = 'nigeld' #Change this to your username
+psql_db = 'nigeld' #Change this to your personal DB name
+psql_password = 'admin' #Put your password (as a string) here
+psql_server = 'studdb2.csc.uvic.ca'
+psql_port = 5432
+
+conn = psycopg2.connect(dbname=psql_db,user=psql_user,password=psql_password,host=psql_server,port=psql_port)
+
+cursor = conn.cursor()
+
 if len(sys.argv) < 2:
 	print("Usage: %s <input file>",file=sys.stderr)
 	sys.exit(0)
@@ -27,4 +37,28 @@ with open(input_filename) as f:
 		#Do something with the data here
 		#Make sure to catch any exceptions that occur and roll back the transaction if a database error occurs.
 		
-		
+		try:
+			for prereq in prerequisites:
+				cursor.execute("insert into students (student_id, name) values( %s, %s );", [student_id, student_name] )
+				# cursor.execute("insert into course_offerings (code, term_code) values( %s, %s );", [code, term] )
+				conn.commit() #Only commit if no error occurs (commit will actually be prevented if an error occurs anyway)
+		except psycopg2.ProgrammingError as err: 
+			#ProgrammingError is thrown when the database error is related to the format of the query (e.g. syntax error)
+			print("Caught a ProgrammingError:",file=sys.stderr)
+			print(err,file=sys.stderr)
+			conn.rollback()
+		except psycopg2.IntegrityError as err: 
+			#IntegrityError occurs when a constraint (primary key, foreign key, check constraint or trigger constraint) is violated.
+			print("Caught an IntegrityError:",file=sys.stderr)
+			print(err,file=sys.stderr)
+			conn.rollback()
+		except psycopg2.InternalError as err:  
+			#InternalError generally represents a legitimate connection error, but may occur in conjunction with user defined functions.
+			#In particular, InternalError occurs if you attempt to continue using a cursor object after the transaction has been aborted.
+			#(To reset the connection, run conn.rollback() and conn.reset(), then make a new cursor)
+			print("Caught an IntegrityError:",file=sys.stderr)
+			print(err,file=sys.stderr)
+			conn.rollback()
+	
+cursor.close()
+conn.close()	
