81faa79fb59deebef79f1a94a1168618f42a59c5
KoanMK <nigeldecontie@gmail.com>
2018-04-08 20:58:56 -0700

    yuh

--- A4/report_enrollment.py
+++ A4/report_enrollment.py
@@ -9,11 +9,21 @@
 
 import psycopg2, sys
 
+psql_user = 'nigeld' #Change this to your username
+psql_db = 'nigeld' #Change this to your personal DB name
+psql_password = 'admin' #Put your password (as a string) here
+psql_server = 'studdb2.csc.uvic.ca'
+psql_port = 5432
+
+conn = psycopg2.connect(dbname=psql_db,user=psql_user,password=psql_password,host=psql_server,port=psql_port)
+
+cursor = conn.cursor()
+
 def print_row(term, course_code, course_name, instructor_name, total_enrollment, maximum_capacity):
 	print("%6s %10s %-35s %-25s %s/%s"%(str(term), str(course_code), str(course_name), str(instructor_name), str(total_enrollment), str(maximum_capacity)) )
 
 # Mockup: Print some data for a few made up classes
 
-print_row(201709, 'CSC 106', 'The Practice of Computer Science', 'Bill Bird', 203, 215)
-print_row(201709, 'CSC 110', 'Fundamentals of Programming: I', 'Jens Weber', 166, 200)
-print_row(201801, 'CSC 370', 'Database Systems', 'Bill Bird', 146, 150)
+# print_row(201709, 'CSC 106', 'The Practice of Computer Science', 'Bill Bird', 203, 215)
+# print_row(201709, 'CSC 110', 'Fundamentals of Programming: I', 'Jens Weber', 166, 200)
+# print_row(201801, 'CSC 370', 'Database Systems', 'Bill Bird', 146, 150)
