102733065c4fbb4d32a839f070a4e74bab22622c
KoanMK <nigeldecontie@gmail.com>
2018-04-08 18:23:37 -0700

    yuh

--- A4/create_courses.py
+++ A4/create_courses.py
@@ -13,6 +13,8 @@ psql_port = 5432
 
 conn = psycopg2.connect(dbname=psql_db,user=psql_user,password=psql_password,host=psql_server,port=psql_port)
 
+cursor = conn.cursor()
+
 if len(sys.argv) < 2:
 	print("Usage: %s <input file>",file=sys.stderr)
 	sys.exit(0)
